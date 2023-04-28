e6d8bbed13c239c368b7445580e4fa4a4fc20700
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-04-28 00:06:53 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        double n1 = 10;
+        double n2 = 30;
+
+        sum(20, 55); // chamando a função no programa
+
+    }
+
+    // declarando a função
+    static void sum(int x, int y) {
+
+        System.out.println(x + y);
+
+    }
+
+}
+
+
