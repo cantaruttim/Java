030f2e47bffbe286fc267d3b3ce643a8d2c96cc9
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-04-29 11:01:05 -0300

    Add files via upload

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,58 @@
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.print("Bem-vindo(a) à Calculadora JAVA! \n");
+
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+
+        if(args[0].equals("somar")) {
+            // Sum
+            sum(x, y);
+        } else if (args[0].equals("subtrair")) {
+
+            // Subtraction
+            sub(x, y);
+        } else if ( args[0].equals("multiplicar") ) {
+
+            //multiply
+            multiply(x, y);
+        } else if (args[0].equals("dividir")) {
+
+            //div
+            div(x, y);
+
+        } else {
+            System.out.println("Nenhuma Instrução Definida");
+        }
+
+
+    }
+
+    static void sum(int x, int y) {
+
+        System.out.println(x + y);
+
+    }
+
+    static void sub(int x, int y) {
+
+        System.out.println(x - y);
+
+    }
+
+    static void multiply(int x, int y) {
+
+        System.out.println(x * y);
+
+    }
+
+    static void div(int x, int y) {
+
+        System.out.println(x / y);
+
+    }
+
+
+}
