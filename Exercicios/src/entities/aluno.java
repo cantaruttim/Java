2fa9eccd86d02b317bc188ab55db557ced099a2d
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-05-08 08:40:21 -0300

    Add files via upload

---
+++ Exercicios/src/entities/aluno.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class aluno {
+	
+	public String name;
+	public double nota_1t;
+	public double nota_2t;
+	public double nota_3t;
+	
+	public double soma() {
+		return nota_1t + nota_2t + nota_3t;
+	}
+	
+	
+
+}
